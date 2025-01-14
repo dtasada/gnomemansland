daf1bee52c1a67870572db785e9d8a6feca1fab2
LMCuber <leo.bozkir@outlook.com>
2025-01-14 11:05:41 +0100

    daniel is an idot

--- include/engine.hpp
+++ include/engine.hpp
@@ -4,8 +4,8 @@
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_net.h>
 #elif __APPLE__
-#include <SDL2/SDL_image.h>
-#include <SDL2/SDL_net.h>
+#include <SDL_image.h>
+#include <SDL_net.h>
 #endif
 
 int randint(int min, int max);
