8ea48a6cbc28a1a057f1865e398d0a6c0cb66e4b
LMCuber <leo.bozkir@outlook.com>
2025-01-24 16:24:17 +0100

    asd

--- client/include/engine.hpp
+++ client/include/engine.hpp
@@ -25,29 +25,6 @@ float rand01();
 v3 lerp_color(v3 c1, v3 c2, float m);
 float rand01();
 
-<<<<<<< HEAD:include/engine.hpp
-// namespace Color {
-//     inline v3 WATER_LOW = v3(0, 0, 50);
-//     inline v3 WATER_HIGH = v3(30, 110, 140);
-//     inline v3 SAND_LOW = v3(237, 206, 178);
-//     inline v3 SAND_HIGH = v3(255, 245, 193);
-//     inline v3 GRASS_LOW = v3(0, 140, 90);
-//     inline v3 GRASS_HIGH = v3(0, 120, 80);
-//     inline v3 MOUNTAIN_LOW = v3(80, 80, 80);
-//     inline v3 MOUNTAIN_HIGH = v3(120, 120, 120);
-// };
-
-struct Color {
-    static v3 WATER_LOW;
-    static v3 WATER_HIGH;
-    static v3 SAND_LOW;
-    static v3 SAND_HIGH;
-    static v3 GRASS_LOW;
-    static v3 GRASS_HIGH;
-    static v3 MOUNTAIN_LOW;
-    static v3 MOUNTAIN_HIGH;
-};
-=======
 inline namespace Color {
 inline v3 WATER_LOW = v3(0, 0, 50);
 inline v3 WATER_HIGH = v3(30, 110, 140);
@@ -58,7 +35,6 @@ inline v3 GRASS_HIGH = v3(0, 120, 80);
 inline v3 MOUNTAIN_LOW = v3(80, 80, 80);
 inline v3 MOUNTAIN_HIGH = v3(120, 120, 120);
 }; // namespace Color
->>>>>>> 6b01fd65cb64f4b7d02199ed09f385304c1b6f5d:client/include/engine.hpp
 
 struct Settings {
     struct {
